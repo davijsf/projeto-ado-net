f2158c2a53766138c8dd548941381a6f495d73c0
teste <teste>
2026-04-17 20:35:04 -0300

    "feat: implement ClienteService "

--- Services/ClienteService.cs
+++ Services/ClienteService.cs
@@ -1,10 +1,35 @@
 namespace Services;
 
+using System.Data.SqlTypes;
 using System.Security;
 using Models;
 
+using MySqlConnector;
+
 public class ClienteService
 {
-    // public void CadastrarCliente(string nome, string cpf, string email);
 
+    private readonly string _connectionString = 
+                        "server=localhost;" +
+                       "database=livraria_ado_net;" +
+                       "uid=root;" +
+                       "pwd=1234";
+
+    public void CadastrarCliente(string nome, string cpf, string email)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+
+            string sql = "INSERT INTO cliente (nome, cpf, email) VALUES (@nome, @cpf, @email)";
+
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+            {
+                cmd.Parameters.AddWithValue("@nome", nome);
+                cmd.Parameters.AddWithValue("@cpf", cpf);
+                cmd.Parameters.AddWithValue("@email", email);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
 }
