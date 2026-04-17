b39aca4bff61fd77659724f8df6faa8df26921f3
davijsf <davif2717@gmail.com>
2026-04-17 14:53:41 -0300

    fix: remove em texto de test. não necessário mais.

--- ConsoleApp/Application.cs
+++ ConsoleApp/Application.cs
@@ -7,22 +7,7 @@ using Data;
 class Application {
 
     public static void Main(string[] args) {
-
         DataBaseConnection db = new DataBaseConnection();
-
-
-        // Teste:
-         db.ExecutarComando(
-        "INSERT INTO cliente (nome, cpf, email) VALUES (@NOME, @EMAIL, @CPF)",
-            new Dictionary<string, object> {
-                { "@NOME",  "Davi"},
-                { "@EMAIL", "davi@email.com"},
-                { "@CPF", "777.111.000-65"}
-            }
-        );
-
-        DataTable clientes = db.PreencherTabela("SELECT * FROM clientes");
-        Console.WriteLine($"Conexão OK! Total de clientes: {clientes.Rows.Count}");
     }
 }
 
