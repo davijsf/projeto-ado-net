7671f9f2777d176bd279ed3278d925f2fc144b21
MateusDv27 <carlosmateusgames604@gmail.com>
2026-04-17 21:23:14 -0300

    AutorService adicionado

---
+++ Services/AutorService.cs
@@ -0,0 +1,92 @@
+namespace Services;
+
+using Models;
+using MySqlConnector;
+
+public class AutorService
+{
+    private readonly string _connectionString = 
+                        "server=localhost;" +
+                       "database=livraria_ado_net;" +
+                       "uid=root;" +
+                       "pwd=1234";
+
+    public void CadastrarAutor(string nome, string nacionalidade)
+    {
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string sql = "INSERT INTO autor (nome, nacionalidade) VALUES (@nome, @nacionalidade)";
+
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+            {
+                cmd.Parameters.AddWithValue("@nome", nome);
+                cmd.Parameters.AddWithValue("@nacionalidade", nacionalidade);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public List<Autor> ListarAutores()
+    {
+        List<Autor> autores = new List<Autor>();
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string sql = "SELECT id, nome, nacionalidade FROM autor";
+
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+            {
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Autor autor = new Autor
+                        {
+                            Id = reader.GetInt32("id"),
+                            Nome = reader.GetString("nome"),
+                            Nacionalidade = reader.IsDBNull(reader.GetOrdinal("nacionalidade")) ? null : reader.GetString("nacionalidade")
+                        };
+                        autores.Add(autor);
+                    }
+                }
+            }
+        }
+        return autores;
+    }
+
+    public void AtualizarAutor(int id, string nome, string nacionalidade)
+    {
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string sql = "UPDATE autor SET nome = @nome, nacionalidade = @nacionalidade WHERE id = @id";
+
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@nome", nome);
+                cmd.Parameters.AddWithValue("@nacionalidade", nacionalidade);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void DeletarAutor(int id)
+    {
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string sql = "DELETE FROM autor WHERE id = @id";
+
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
