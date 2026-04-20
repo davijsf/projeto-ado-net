4d759969de6991c60cf3bce06912f9f30bd0632a
Lusiane Lima <lusianelima5@gmail.com>
2026-04-20 11:16:50 -0300

    feat:  implement IAutor

---
+++ Business/Interfaces/IAutor.cs
@@ -0,0 +1,10 @@
+﻿using Entities;
+namespace Business.Interfaces;
+public interface IAutorService
+{
+    void CadastrarAutor(Autor autor);
+    List<Autor> ListarAutores();
+    void AtualizarAutor(Autor autor);
+    void RemoverAutor(int id);
+}
+
