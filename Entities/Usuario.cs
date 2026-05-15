35afb153cf5805ef553807b401523e5cfbc071d9
davijsf <davif2717@gmail.com>
2026-05-15 16:19:15 -0300

    fix: Alteração de tipo: string --> Enum NivelAcesso em atributo 'nivel' do usuario

--- Entities/Usuario.cs
+++ Entities/Usuario.cs
@@ -6,7 +6,7 @@ public class Usuario
     public string ? Username { get ; set ; }
     public string ? Senha { get ; set ; }
     
-    public string ? nivel { get ; set ; }
+    public NivelAcesso nivel { get ; set ; }
     // Incerto sobre o tipo do avatar
     public string ? Avatar { get ; set ; }
 }
