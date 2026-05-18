03bc6e9586fec0922c906e12bfaeffbcca4d9683
Lusiane Lima <lusianelima5@gmail.com>
2026-05-17 21:22:37 -0300

    feat: controle de acesso por nivel no menu principal

--- ConsoleApp/Application.cs
+++ ConsoleApp/Application.cs
@@ -1,35 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-namespace ConsoleApp;
+﻿namespace ConsoleApp;
 
-using Data;
-using Services;
 using ConsoleApp.Menus;
+using Data;
+using Entities;
+using Services;
 
-class Application {
-
-    public static void Main(string[] args) {
-        DataBaseConnection db = new DataBaseConnection();
-
-        ClienteService clienteService = new ClienteService();
+class Application
+{
+    public static void Main(string[] args)
+    {
         UsuarioService usuarioService = new UsuarioService();
-        VendedorService vendedorService = new VendedorService();
         LivroService livroService = new LivroService();
-        VendaService vendaService = new VendaService();
         AutorService autorService = new AutorService();
+        VendaService vendaService = new VendaService();
+        VendedorService vendedorService = new VendedorService();
+        ClienteService clienteService = new ClienteService();
 
-        
         LivroMenu livroMenu = new LivroMenu(livroService, autorService);
-        UsuarioMenu usuarioMenu = new UsuarioMenu(usuarioService); 
-        VendaMenu vendaMenu = new VendaMenu(livroService, vendaService);
+        UsuarioMenu usuarioMenu = new UsuarioMenu(usuarioService);
+        VendaMenu vendaMenu = new VendaMenu(livroService, vendaService, clienteService);
+        AutorMenu autorMenu = new AutorMenu(autorService);
+        VendedorMenu vendedorMenu = new VendedorMenu(vendedorService, usuarioService);
+        ClienteMenu clienteMenu = new ClienteMenu(clienteService, usuarioService);
 
-        bool sair = false;
-        while (!sair)
+        // Login antes de acessar o sistema
+        Usuario? usuarioLogado = null;
+
+        while (usuarioLogado == null)
         {
             Console.Clear();
             Console.WriteLine("=== LIVRARIA ===");
-            Console.WriteLine("1. Livros");
-            Console.WriteLine("2. Usuário");
-            Console.WriteLine("3. Venda");
+            Console.WriteLine("1. Login");
             Console.WriteLine("0. Sair");
             Console.Write("\nEscolha uma opção: ");
 
@@ -37,18 +38,71 @@ class Application {
 
             switch (opcao)
             {
-                case "1": livroMenu.ExibirMenu(); break;
-                case "2": usuarioMenu.ExibirMenu(); break;
-                case "3": vendaMenu.ExibirMenu(); break;
-                case "0": sair = true; break;
+                case "1":
+                    usuarioLogado = usuarioMenu.Login();
+                    break;
+                case "0":
+                    return;
                 default:
                     Console.WriteLine("Opção inválida!");
                     Console.ReadKey();
                     break;
             }
         }
-        
 
+        // Menu principal baseado no nível de acesso
+        bool sair = false;
+        while (!sair)
+        {
+            Console.Clear();
+
+            if (usuarioLogado.nivel == NivelAcesso.Admin)
+            {
+                Console.WriteLine($"=== LIVRARIA [ADMIN] - {usuarioLogado.Username} ===");
+                Console.WriteLine("1. Livros");
+                Console.WriteLine("2. Clientes");
+                Console.WriteLine("3. Vendedores");
+                Console.WriteLine("4. Autores");
+                Console.WriteLine("5. Vendas");
+                Console.WriteLine("6. Usuários");
+                Console.WriteLine("0. Sair");
+                Console.Write("\nEscolha uma opção: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1": livroMenu.ExibirMenu(); break;
+                    case "2": clienteMenu.ExibirMenu(); break;
+                    case "3": vendedorMenu.ExibirMenu(); break;
+                    case "4": autorMenu.ExibirMenu(); break;
+                    case "5": vendaMenu.ExibirMenu(usuarioLogado); break;
+                    case "6": usuarioMenu.ExibirMenu(); break;
+                    case "0": sair = true; break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"=== LIVRARIA [COMUM] - {usuarioLogado.Username} ===");
+                Console.WriteLine("1. Vendas");
+                Console.WriteLine("2. Meu Perfil");
+                Console.WriteLine("0. Sair");
+                Console.Write("\nEscolha uma opção: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1": vendaMenu.ExibirMenu(usuarioLogado); break;
+                    case "2": usuarioMenu.ExibirPerfil(usuarioLogado); break;
+                    case "0": sair = true; break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
     }
 }
 
